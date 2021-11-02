8d452d37e34287bfc3e8cfb29437b2f3686ac78a
AYSHA ELSHERIF <a.elsherif.20@abdn.ac.uk>
2021-11-02 18:56:39 +0000

    finished task 2.1 and 2.2

--- src/main/java/uoa/assignment1/game/Map.java
+++ src/main/java/uoa/assignment1/game/Map.java
@@ -5,15 +5,30 @@ import uoa.assignment1.character.Monster;
 import uoa.assignment1.character.Player;
 
 public class Map {
-
-public String [][] layout;
-public GameCharacter characters [] ;
+    public String[][] layout;
+    public GameCharacter[] characters = new GameCharacter[4];
+    
+    public Map (int height, int width) {
+        layout = new String[height][width];
+    }
+    
+    //code to intialise array adapted from: https://www.codegrepper.com/code-examples/java/populate+2d+array+java
+    private void initialiseArray() {
+        for (int height = 0; height < layout.length; height++) {
+            for (int width = 0; width < layout[height].length; width++) {
+                layout[height][width] = ".";
+            }
+        }
+    }
  
-  Map (int height, int width) {
-	 
-  }
- 
-  public void printLayout() {
-	
-   }
+    //code to print array adapted from: https://www.codegrepper.com/code-examples/java/how+to+print++a+2d+array+in+java
+    public void printLayout() {
+        initialiseArray();
+        for (int height = 0; height < layout.length; height ++) {
+            for (int width = 0; width < layout[height].length; width++) {
+                System.out.print(layout[height][width]);
+            }
+            System.out.println();
+        }
+    }   
 }
